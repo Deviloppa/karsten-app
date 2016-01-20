4515ef7178266dd946b96df00838da7a7096d2c9
Karsten Lam <karsten.lam@gmail.com>
2016-01-20 19:14:15 +0100

    test

---
+++ test.js
@@ -0,0 +1,21 @@
+if(Meteor.isClient){
+  Template.takePhoto.events({
+    'click .capture': function(){
+      console.log("Button clicked.");
+      MeteorCamera.getPicture({}, function(error, data){
+        Session.set('photo', data);
+      });
+    }
+  });
+  Template.takePhoto.helpers({
+    'photo': function(){
+      return Session.get('photo');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
